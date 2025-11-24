b08241051b86355ae2c19a4bc9a1bc8d209062b5
HopHouse13 <pierrealexisbret@gmail.com>
2025-11-24 13:10:17 +0100

    * maj

---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbret <pbret@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 16:24:19 by pbret             #+#    #+#             */
+/*   Updated: 2025/11/20 17:21:56 by pbret            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class	fixed
+{
+	public :
+		
+
+	private:
+		int	 fixedPointNb;
+		//int const static	
+};
+
+
+//class Human
+//{
+//	private:
+//		int _age;
+//	public:
+//		Human();
+//		Human(int age);
+//		Human(const Human &copy);
+//		Human &operator=(const Human &rhs);
+//		~Human();
+//};
+
+//Human::Human() : _age(0)
+//{
+//}
+
+//Human::Human(int age) : _age(age)
+//{
+//}
+
+//Human::Human(const Human &copy) : _age(copy._age)
+//{
+//}
+
+//Human &Human::operator=(const Human &rhs)
+//{
+//	if (this != &rhs)
+//	{
+//		_age = rhs._age;
+//	}
+//	return (*this);
+//}
+
+//Human::~Human()
+//{
+//}
+
+#endif 
