5168bc27e5116d65c260cd2e409a9c3b8cf358fb
HopHouse13 <pierrealexisbret@gmail.com>
2025-12-11 19:13:41 +0100

    * les exceptions

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,25 +6,87 @@
 /*   By: pbret <pbret@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 18:41:19 by pbret             #+#    #+#             */
-/*   Updated: 2025/12/10 19:07:23 by pbret            ###   ########.fr       */
+/*   Updated: 2025/12/11 19:09:12 by pbret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(void) : _name("unknown"), _grade(0) // grade entre 1 - 150
-{
-	std::cout << "Default constructor called" << std::endl;
+Bureaucrat::Bureaucrat(void) : _name("unknown"), _grade(150) // grade entre 1 - 150
+{	
+	std::cout << "Default constructor of " << this->_name << " called" << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat& rhs)
-{
-	this->_name = rhs._name;
-	this->_grade = rhs._grade;
+Bureaucrat::Bureaucrat(const std::string name, const unsigned int grade) : _name(name)
+{	
+	std::cout << "Constructor of " << this->_name << " called" << std::endl;
+	try
+	{
+		if(grade < 1)
+			throw Bureaucrat::GradeTooLowException(); // c'est quoi exactement? un constructeur?
+		if(grade > 150)
+			throw Bureaucrat::GradeTooHighException();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what();
+	}
+	this->_grade = grade;
 }
-	//Bureaucrat& operator=(const Bureaucrat& rhs);
-	//~Bureaucrat(void);
-	//const std::string&	getName(void) const;
-	//const unsigned int&	getGrade(void) const;
-	//void				incrementGrade(void);
-	//void				decrementGrade(void);
+
+Bureaucrat::Bureaucrat(const Bureaucrat& rhs) : _name(rhs._name), _grade(rhs._grade)
+{
+	std::cout << "Copy constructor of " << this->_name << " called" << std::endl;
+}
+
+Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& rhs)
+{
+	std::cout << "Assignment operator of " << this->_name << " overload called" << std::endl;
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_grade = rhs._grade;
+	}
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+	std::cout << "Destructor of " << this->_name << " called" << std::endl;
+}
+
+const std::string&	Bureaucrat::getName(void) const
+{
+	return (this->_name);
+}
+
+const unsigned int&	Bureaucrat::getGrade(void) const
+{
+	return (this->_grade);
+}
+
+void	Bureaucrat::incrementGrade(void)
+{
+	this->_grade++;
+}
+
+void	Bureaucrat::decrementGrade(void)
+{
+	this->_grade--;
+}
+
+const char	*Bureaucrat::GradeTooHighException::what(void) const throw()
+{
+	return(RED"Grade is too high\n"RESET);
+}
+
+const char	*Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+	return(RED"Grade is too low\n"RESET);
+}
+
+std::ostream&	operator<<(std::ostream& outStream, const Bureaucrat& rhs)
+{
+	outStream << YELLOW << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << "." << RESET << std::endl;
+	return (outStream);
+}
--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: pbret <pbret@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 18:41:22 by pbret             #+#    #+#             */
-/*   Updated: 2025/12/10 19:05:22 by pbret            ###   ########.fr       */
+/*   Updated: 2025/12/11 19:05:55 by pbret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,21 @@
 #include <string>
 #include <iostream>
 
+# define RESET "\033[0m"
+# define BLACK "\033[30m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define WHITE "\033[37m"
+
 class Bureaucrat
 {
 	public:
 		Bureaucrat(void);
+		Bureaucrat(const std::string name, const unsigned int grade);
 		Bureaucrat(const Bureaucrat& rhs);
 		Bureaucrat& operator=(const Bureaucrat& rhs);
 		~Bureaucrat(void);
@@ -29,12 +40,23 @@ class Bureaucrat
 		void				incrementGrade(void);
 		void				decrementGrade(void);
 
-	private:
-		const std::string	_name;
-		unsigned int		_grade;
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char	*what(void) const throw(); // throw() assure au compilateur que la fonction ne renvoie pas une exception.
+		};
 
-		void	GradeTooHighException(void);
-		void	GradeTooLowException(void);
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char	*what(void) const throw(); // du coup il faudrait le mettre a toutes les fonctions sans throw externe?
+		};
+
+	private:
+		std::string			_name;
+		unsigned int		_grade;
 };
+
+std::ostream&		operator<<(std::ostream& outStream, const Bureaucrat& rhs);
 
 #endif
